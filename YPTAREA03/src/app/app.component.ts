e1685274d4acf723c1778753dc390ea39c6b24da
yohanny <yohannyp@gmail.com>
2022-02-08 20:22:50 -0500

    YPTAREA03_20220207
    ITLAT12022_AngularYohannyPena

---
+++ YPTAREA03/src/app/app.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'YohannyPeñaTarea03';
+  subTitle = 'Clase Directivas-Estructurales (Interpolacion de String)';
+}
